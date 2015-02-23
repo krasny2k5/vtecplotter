3d9e3eed195c0db11d60d9a34d49b398ce323fb1
Jescayo <jescayo@ucm.es>
2015-02-23 10:59:03 +0100

    Solucionados errores con nombres de archivos

--- gg2db.sh
+++ gg2db.sh
@@ -2,27 +2,27 @@
 #Este programa convierte la salida del programa de Gigi en un archivo de tres columnas donde la columna 1 contiene el día del año y el año,
 #la columna dos la hora en formato HH:MM:SS, la columna tres la hora en segundos y la columna 4 el valor de vTEC.
 #Los datos deben encontrarse en una capeta, y se debe llamar al conversor desde el directorio inmediatamente superior.
-#Version 2.0
+#Version 2.1
 #
 #TO-DO: comprobar si el archivo que intentas convertir tiene el formato correcto, y si no es así dar un error.
 #
 #Joaquín Escayo Menéndez Febrero 2015
+#changelog: Solucionado un error al utilizar directorios de más de 4 caracteres, añadidas opciones de debugging (si $debug=1)
 
 echo "Hay que llamar gg2db con el argumento del directorio que quieras convertir"
 
 FILES=./$1/*
-
+debug=0
 total=0
 
 for f in $FILES
 do
-	echo "Procesando el archivo $f"
+	file=$(basename $f)
+	echo "Procesando el archivo $file"
 	#Me quedo con el día del año y lo almaceno en dia
-	dia=$(echo $f | eval sed 's/..............//' | eval sed 's/....$//')
-	#almaceno el nombre del archivo
-	archivo=$(echo $f | eval sed 's/.......//')
+	dia=$(echo $file | eval sed 's/.......//' | eval sed 's/....$//')
 	#almaceno el año del archivo:
-	yearshort=$(echo $f | eval sed 's/..................//' | eval sed 's/.$//')
+	yearshort=$(echo $file | eval sed 's/...........//' | eval sed 's/.$//')
 	if [ $yearshort -gt 60 ]; then
         	yearlong=$(echo "19$yearshort")
 	else
@@ -35,6 +35,13 @@ do
 	#sobreescribo el archivo original:
 	mv $f.tmp $f
 	total=$((total+1))
+	if [ $debug == 1 ]; then
+		echo "Nombre del archivo: $file"
+		echo "Día: $dia"
+		echo "Año corto: $yearshort"
+		echo "Año largo: $yearlong"
+		echo "---------------------"
+	fi
 done
 
 echo "Se ha procesador un total de $total archivos"
